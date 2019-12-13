c7d125965873840d5b374c541482bed9907ccf6f
WataruTakahasi <zzzooozzzooo1011@gmail.com>
2019-12-13 02:34:59 +0000

    almost finish

--- app/assets/javascripts/history_img.js
+++ app/assets/javascripts/history_img.js
@@ -22,7 +22,7 @@ $(function(){
       var imgPos = $(this).offset().top;    
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight + windowHeight/5){
+      if (scroll > imgPos - windowHeight + windowHeight/7){
         $("#history2",this).css("opacity","1" );
         $("#history2",this).fadeIn();
       } else {
@@ -39,12 +39,26 @@ $(function(){
       var imgPos = $(this).offset().top;    
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight + windowHeight/5){
+      if (scroll > imgPos - windowHeight + windowHeight/100){
         $("#history3",this).css("opacity","1" );
         $("#history3",this).fadeIn();
       } else {
         $("#history3",this).css("opacity","0" );
         $("#history3",this).fadeOut();
+      }
+    });
+  });
+});
+$(function(){
+  $('.history_line').css("opacity","0");
+  $(window).scroll(function (){
+    $(".history_line_div").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/100){
+        $('.history_line').css("opacity","1");
+        $(".history_line").addClass("history_line2");
       }
     });
   });
---
+++ app/assets/javascripts/slideInOut.js
@@ -0,0 +1,6 @@
+$(document).ready( function(){
+  // $('.first_imp').addClass("slideinout);
+  setTimeout(function(){
+        $(".first_imp").css({display:'none'},3000);
+    },2500);
+});
